7e156810a5f4db38e2eec60256ea801a70f7b084
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-07-24 19:35:31 -0400

    Test: two different lists are unequal

--- sublist.js
+++ sublist.js
@@ -3,8 +3,12 @@ class List {
         this.content = content || [];
         this.length = this.content.length;
     }
+    arraysAreEqual(list1, list2) {
+        return list1.filter((element, index) => element === list2[index]).length === list1.length;
+    }
     compare(anotherList) {
         if (anotherList.length === this.length) {
+            if (!this.arraysAreEqual(this.content, anotherList.content)) return 'UNEQUAL';
             return 'EQUAL';
         } else if (anotherList.length > this.length){
             return 'SUBLIST';
