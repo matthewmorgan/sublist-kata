db475af0d8850c065fc6755b42d752b2f0982023
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-07-24 19:06:56 -0400

    Test: an empty list is a sublist of a non-empty list

--- sublist.js
+++ sublist.js
@@ -1,7 +1,15 @@
 class List {
+    constructor(content) {
+        this.content = content || [];
+        this.length = this.content.length;
+    }
     compare(anotherList) {
+        if (anotherList.length > 0){
+            return 'SUBLIST';
+        }
         return 'EQUAL';
-    };
+
+    }
 }
 
 export default List;
